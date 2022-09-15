aa3cba60fd9a337c0d0ab88dae49fee0214ac9a5
peytonblake <peytonsblake@gmail.com>
2022-09-15 13:04:37 -0500

    Started title page

---
+++ src/components/titleScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TitleContainer, TitleText } from './titleScreen';
+
+const TitleScreen = () => {
+  return (
+    <>
+      <TitleContainer>
+        <TitleText>JOELOPOLY</TitleText>
+      </TitleContainer>
+    </>
+  );
+};
+
+export default TitleScreen;
---
+++ src/components/titleScreen/titleScreen.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const TitleContainer = styled.div`
+  background: #2c3325;
+  display: flex;
+  justify-content: center;
+  position: sticky;
+  height: 100vh;
+`;
+
+export const TitleText = styled.h1`
+  color: #fef9f3;
+  font-weight: bold;
+  line-height: 10px;
+  text-align: center;
+  font-size: 8rem;
+  position: relative;
+  margin-top: 20vh;
+`;
