19aade71817d2514efabb01b654dc54f6e70855d
peytonblake <peytonsblake@gmail.com>
2022-09-20 15:57:29 -0500

    Full title screen converted to typescript

--- src/components/titleScreen/index.tsx
+++ src/components/titleScreen/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TitleScreenContainer,
   TitleContainer,
   TitleText,
   ButtonWrapper,
+  PlayerWrapper,
+  PlayerText,
+  PlayerButton,
 } from './titleScreen';
 import { Button } from '../button';
+
+const types = ['1', '2', '3', '4'];
+function ToggleGroup() {
+  const [active, setActive] = useState(types[0]);
+  return (
+    <PlayerWrapper>
+      <PlayerText>PLAYERS</PlayerText>
+      {types.map((type) => (
+        <PlayerButton onClick={() => setActive(type)} active={active === type}>
+          {type}
+        </PlayerButton>
+      ))}
+    </PlayerWrapper>
+  );
+}
 
 const TitleScreen = () => {
   return (
@@ -14,6 +32,7 @@ const TitleScreen = () => {
         <TitleContainer>
           <TitleText>JOELOPOLY</TitleText>
         </TitleContainer>
+        <ToggleGroup />
         <ButtonWrapper>
           <Button to="/">START</Button>
         </ButtonWrapper>
--- src/components/titleScreen/titleScreen.tsx
+++ src/components/titleScreen/titleScreen.tsx
@@ -1,34 +1,58 @@
 import styled from 'styled-components';
 
+interface ButtonI {
+  active: boolean
+}
+
 export const TitleScreenContainer = styled.div`
   background: #2c3325;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 `;
 
 export const TitleContainer = styled.div`
-  display: flex;
   justify-content: center;
-  position: absolute;
+  margin-bottom: 10vh;
 `;
 
 export const TitleText = styled.h1`
   color: #fef9f3;
   font-weight: bold;
   line-height: 10px;
-  text-align: center;
   font-size: 8rem;
-  position: relative;
-  margin-top: 20vh;
 `;
 
 export const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  position: absolute;
-  margin-top: 70vh;
+  position: relative;
   height: 10vh;
   font-weight: bold;
+  margin-bottom: 20vh;
+`;
+
+export const PlayerWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 10vh;
+  margin-bottom: 10vh;
+`;
+
+export const PlayerText = styled.h1`
+  color: #fef9f3;
+  font-weight: bold;
+  font-size: 3rem;
+`;
+
+export const PlayerButton = styled.button<ButtonI>`
+  background-color: ${({ active }) => (active ? 'green' : 'grey')};
+  color: #fef9f3;
+  padding: 10px 10px;
+  font-size: 20px;
+  cursor: pointer;
+  height: 8vh;
+  width: 8vh;
 `;
