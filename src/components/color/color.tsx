042c64e555eb60d8a3aaa8d70fa1c37ee15dbdc4
peytonblake <32944116+peytonblake@users.noreply.github.com>
2022-09-21 16:48:52 -0500

    Finished color screen design, just needs functionality

--- src/components/color/color.tsx
+++ src/components/color/color.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+interface ButtonI {
+  active: boolean,
+  color: string
+};
+
 export const ColorContainer = styled.div`
   background: #2c3325;
   height: 100vh;
@@ -7,4 +12,59 @@ export const ColorContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const PlayerNameContainer = styled.div`
+  justify-content: center;
+  margin-bottom: 10vh;
+`;
+
+export const PlayerName = styled.h1`
+  color: #fef9f3;
+  font-weight: bold;
+  line-height: 10px;
+  font-size: 8rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  position: relative;
+  height: 10vh;
+  font-weight: bold;
+  margin-bottom: 20vh;
+`;
+
+export const ColorChooseContainer = styled.div`
+  position: relative;
+`;
+
+export const ColorText = styled.h1`
+  color: #fef9f3;
+  font-weight: bold;
+  line-height: 10px;
+  font-size: 2rem;
+`;
+
+export const ColorButtonContainer = styled.div`
+  position: relative;
+  height: 20vh;
+  margin-bottom: 10vh;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const ColorButton = styled.button<ButtonI>`
+  background-color: ${({ active, color }) => (active ? color : 'grey')};
+  color: #fef9f3;
+  padding: 10px 10px;
+  font-size: 20px;
+  cursor: pointer;
+  height: 8vh;
+  width: 8vh;
+  border: none;
+
+  &:hover {
+    transform: scale(1.05, 1.05);
+    box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.5);
+  }
 `;
--- src/components/color/index.tsx
+++ src/components/color/index.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
-import { ColorContainer} from './color';
+import { ColorContainer, PlayerNameContainer, PlayerName, ButtonWrapper, ColorChooseContainer, ColorButtonContainer, ColorText, ColorButton } from './color';
 import { Button } from '../button';
 
 const Color = () => {
   return (
     <>
-    <ColorContainer />
+      <ColorContainer>
+        <PlayerNameContainer>
+          <PlayerName>PLAYER x</PlayerName>
+        </PlayerNameContainer>
+        <ColorChooseContainer>
+          <ColorText>CHOOSE YOUR COLOR</ColorText>
+        </ColorChooseContainer>
+        <ColorButtonContainer>
+          <ColorButton active={true} color={"green"}/>
+          <ColorButton active={true} color={"red"}/>
+          <ColorButton active={true} color={"yellow"}/>
+          <ColorButton active={true} color={"blue"}/>
+        </ColorButtonContainer>
+        <ButtonWrapper>
+          <Button to="/color">NEXT</Button>
+        </ButtonWrapper>
+      </ColorContainer>
     </>
   );
 };
