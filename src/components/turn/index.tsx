79ad57067becd9913ec7247059c67d861c8c7e3b
peytonblake <32944116+peytonblake@users.noreply.github.com>
2022-10-16 15:11:56 -0500

    Turn page complete

--- src/components/turn/index.tsx
+++ src/components/turn/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { Button } from '../button';
 import numPlayers from '../../game/numPlayers';
 import game from '../../game/game';
+import { TurnWrapper, ResultsWrapper, StartWrapper, StartText, ButtonWrapper } from '../turn/turn';
 
 const Turn = () => {
   game.addAIPlayers(numPlayers.numAIPlayers);
 
   return (
     <>
-      {game.rollForFirst().map((roll, i) => <div>{game.players[i].name} rolled a {roll}</div>)}
-      <div>
-        {game.getCurrentPlayer().name} goes first!
-      </div>
+      <TurnWrapper>
+        <ResultsWrapper>
+          {game.rollForFirst().map((roll, i) => <div><StartText>{game.players[i].name}</StartText><StartText>{roll}</StartText></div>)}
+        </ResultsWrapper>
+        <StartWrapper>
+          <StartText>
+            <div>{game.getCurrentPlayer().name} goes first!</div>
+          </StartText>
+        </StartWrapper>
+        <ButtonWrapper>
+          <Button to="/game">START</Button>
+        </ButtonWrapper>
+      </TurnWrapper>
     </>
   );
 };
--- src/components/turn/turn.tsx
+++ src/components/turn/turn.tsx
@@ -1,2 +1,41 @@
-const todo = "todo";
-export default todo;
+import styled from 'styled-components';
+
+export const TurnWrapper = styled.div`
+  background: #2c3325;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ResultsWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-diection: row;
+  align-items: center;
+  justify-content: space-around;
+  column-gap: 10vh;
+`;
+
+export const StartWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 5vh;
+  margin-bottom: 5vh;
+`;
+
+export const StartText = styled.h1`
+  color: #fef9f3;
+  font-weight: bold;
+  font-size: 3rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  position: relative;
+  height: 10vh;
+  font-weight: bold;
+  margin-bottom: 20vh;
+`;
