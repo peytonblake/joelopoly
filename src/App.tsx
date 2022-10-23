c649918fe702cb8ddd72f525c2a33f94245fb204
unknown <zcb7mf@umsystem.edu>
2022-10-23 17:34:42 -0500

    update added minigame pages to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import TitleScreen from './pages/titleScreen';
 import ColorScreen from './pages/colorScreen'
 import TurnScreen from './pages/turnScreen';
+import DinoScreen from './pages/dinoPage';
+import WhackAMoleScreen from './pages/WhackAMolePage';
+import GuitarHeroScreen from './pages/GuitarHeroPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Route path="/" element={<TitleScreen />} />
         <Route path="/color" element={<ColorScreen />} />
         <Route path="/turn" element={<TurnScreen />} />
+        <Route path="/DinoGame" element={<DinoScreen />} />
+        <Route path="/WhackAMole" element={<WhackAMoleScreen />} />
+        <Route path="/GuitarHero" element={<GuitarHeroScreen />} />
       </Routes>
     </Router>
   );
