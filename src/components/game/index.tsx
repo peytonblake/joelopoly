c4b0ef0977b83e676cabc92872b393f7a08a1245
peytonblake <32944116+peytonblake@users.noreply.github.com>
2022-11-09 15:27:51 -0600

    Revert "netlify fix?"
    This reverts commit 4fc8b71a97e2d1e53556f3fad0addb173b856a9d.

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -2,21 +2,9 @@ import React, { useState } from 'react';
 import monopoly from '../../game/monopoly';
 import MonopolyDisplay from './monopoly';
 import { Player } from '../../game/players';
-import {
-  GameWrapper,
-  SidebarWrapper,
-  RollWrapper,
-  DieWrapper,
-  MiddleWrapper,
-  TitleText,
-  BoardWrapper,
-  RightWrapper,
-  SideInfoBox,
-  SideInfo,
-  SideInfoText,
-  SideInfoBoxLine,
-  PiecesWrapper,
-} from './game';
+import { GameWrapper, SidebarWrapper, RollWrapper, DieWrapper, MiddleWrapper, 
+         TitleText, BoardWrapper, RightWrapper, 
+         SideInfoBox, SideInfo, SideInfoText, SideInfoBoxLine, PiecesWrapper } from './game';
 import { SideInfoCardGetter } from './cards';
 import { Buildings, Players } from './pieces';
 import Dice1 from '../../images/Dice1.png';
@@ -40,109 +28,86 @@ function displayOwnedCards(player: Player, maxWidth: number) {
   }
   return (
     <SideInfoBoxLine>
-      {cardNames.map((cardName) =>
-        SideInfoCardGetter(cardName, maxWidth / cardNames.length)
-      )}
+      {cardNames.map((cardName) => SideInfoCardGetter(cardName, maxWidth / cardNames.length))}
     </SideInfoBoxLine>
-  );
+  )
 }
 
 function displayInfo() {
   return (
     <>
-      <SidebarWrapper>
-        {monopoly.players.map(
-          (player: Player) =>
-            player.alive && (
-              <SideInfoBox
-                key={player.name}
-                active={player == monopoly.getCurrentPlayer()}
-              >
-                <SideInfoBoxLine>
-                  <SideInfo first={true}>
-                    <SideInfoText color={player.color}>
-                      {player.name}
-                    </SideInfoText>
-                  </SideInfo>
-                  <SideInfo first={false}>
-                    <SideInfoText color="#2c3325">${player.money}</SideInfoText>
-                  </SideInfo>
-                </SideInfoBoxLine>
-                {displayOwnedCards(player, 40)}
-              </SideInfoBox>
-            )
-        )}
-      </SidebarWrapper>
+    <SidebarWrapper>
+      {monopoly.players.map((player: Player) => (player.alive && 
+        <SideInfoBox key={player.name} active={player == monopoly.getCurrentPlayer()}>
+          <SideInfoBoxLine>
+            <SideInfo first={true}><SideInfoText color={player.color}>{player.name}</SideInfoText></SideInfo>
+            <SideInfo first={false}><SideInfoText color="#2c3325">${player.money}</SideInfoText></SideInfo>
+          </SideInfoBoxLine>
+          {displayOwnedCards(player, 40)}
+        </SideInfoBox>
+      ))}
+    </SidebarWrapper>
     </>
-  );
+  )
 }
 
 export function getDiceImage(die: number) {
-  switch (die) {
-    case 1: {
-      return <DieWrapper src={Dice1} alt="1" />;
-    }
-    case 2: {
-      return <DieWrapper src={Dice2} alt="2" />;
-    }
-    case 3: {
-      return <DieWrapper src={Dice3} alt="3" />;
-    }
-    case 4: {
-      return <DieWrapper src={Dice4} alt="4" />;
-    }
-    case 5: {
-      return <DieWrapper src={Dice5} alt="5" />;
-    }
-    case 6: {
-      return <DieWrapper src={Dice6} alt="6" />;
-    }
+  switch(die) {
+    case 1: { return <DieWrapper src={Dice1} alt="1" /> }
+    case 2: { return <DieWrapper src={Dice2} alt="2" /> }
+    case 3: { return <DieWrapper src={Dice3} alt="3" /> }
+    case 4: { return <DieWrapper src={Dice4} alt="4" /> }
+    case 5: { return <DieWrapper src={Dice5} alt="5" /> }
+    case 6: { return <DieWrapper src={Dice6} alt="6" /> }
   }
 }
 
 export function displayRoll(die1: number, die2: number) {
   return (
     <>
-      <RollWrapper>
-        {getDiceImage(die1)}
-        {getDiceImage(die2)}
-      </RollWrapper>
+    <RollWrapper>
+      {getDiceImage(die1)}
+      {getDiceImage(die2)}
+    </RollWrapper>
     </>
-  );
+  )
 }
 
 function displayBoard() {
   return (
     <>
-      <MiddleWrapper>
-        <TitleText>JOELOPOLY</TitleText>
-        <PiecesWrapper>
-          <BoardWrapper src={Board} alt="Board" />
-          <Buildings />
-          <Players players={monopoly.players} />
-        </PiecesWrapper>
-      </MiddleWrapper>
+    <MiddleWrapper>
+      <TitleText>JOELOPOLY</TitleText>
+      <PiecesWrapper>
+      <BoardWrapper src={Board} alt="Board"/>
+      <Buildings/>
+      <Players players={monopoly.players}/>
+      </PiecesWrapper>
+      
+    </MiddleWrapper>
     </>
-  );
+  )
 }
 
 const Game = () => {
   const [refresh, setRefresh] = useState(false);
-  function redraw() {
-    setRefresh(!refresh);
-  }
+  function redraw() {setRefresh(!refresh)}
 
   return (
-    <>
+      <>
       <GameWrapper>
         {displayInfo()}
         {displayBoard()}
         <RightWrapper>
-          <MonopolyDisplay redraw={redraw} />
+          <MonopolyDisplay redraw={redraw}/>
         </RightWrapper>
       </GameWrapper>
-    </>
-  );
-};
+      </>
+    );
+  };
+  
+  export default Game;
 
-export default Game;
+
+  
+  
